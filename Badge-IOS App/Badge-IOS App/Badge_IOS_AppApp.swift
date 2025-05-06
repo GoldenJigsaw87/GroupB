1e49cca2c5449cd1cf5a5550f1f7046c8a27603c
Jaydee White <jddubble2@gmail.com>
2025-05-06 08:10:34 -0600

    Creating the IOS app

---
+++ Badge-IOS App/Badge-IOS App/Badge_IOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Badge_IOS_AppApp.swift
+//  Badge-IOS App
+//
+//  Created by Student on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Badge_IOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
